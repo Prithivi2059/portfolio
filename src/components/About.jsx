df36d60eeabbc74e8ff92ecf4ac154323b01b65b
PrithiviPM2580 <prithivipemimagar>
2025-11-10 01:34:57 +0545

    feat: update About, ButtonIcon, and Contact components; enhance styling and layout; improve form accessibility

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <div className="flex flex-col justify-center items-center gap-8 md:gap-10 lg:gap-18">
                 <h1 className="h1">About Me</h1>
                 <div className="relative z-10 flex justify-center items-center">
-                    <p className="relative z-20 w-full md:w-[64%] text-[10px]  md:text-[14px] lg:text-sm text-center">I’m a backend developer who enjoys turning ideas into reliable systems. I love building fast, secure, and scalable APIs that help products run smoothly behind the scenes. I’m passionate about integrating AI into backend systems to create smarter, more efficient, and adaptive solutions</p>
+                    <p className="relative z-20 w-full md:w-[50%] text-[10px]  md:text-[14px] lg:text-sm text-center">I’m a backend developer who enjoys turning ideas into reliable systems. I love building fast, secure, and scalable APIs that help products run smoothly behind the scenes. I’m passionate about integrating AI into backend systems to create smarter, more efficient, and adaptive solutions</p>
                     {buttonIcons.map((icon, index) => (
                         <ButtonIcon key={index} {...icon} />
                     ))}
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,31 +4,30 @@ import ContactCard from "./ContactCard";
 
 const Contact = () => {
     return (
-        <section className="w-full py-8 sm:py-12 bg-red-100">
+        <section className="w-full py-8 sm:py-12">
             <div className="flex flex-col justify-center items-center gap-8 md:gap-10 lg:gap-1">
                 <h1 className="h1">Contact</h1>
-                <div className="w-full flex gap-8">
+                <div className="w-full flex flex-col py-12 md:flex-row gap-8">
                     <div className="flex flex-col gap-4 md:w-1/2">
                         <ButtonIcon name="Contact" icon="/svgs/contact.svg" bg="#E3E3E3" relative="relative" />
                         <h1 className="font-semibold text-base md:text-xl">Get In Touch</h1>
                         <p className="text-gray-custom text-sm">Have a question about me or my work? Feel free to ask!</p>
                         <div className="flex flex-col gap-2">
-                            {
-                                contactCards.map((card) => (
-                                    <ContactCard key={card.id} icon={card.icon} title={card.title} name={card.name} />
-                                ))
-                            }
+                            {contactCards.map((card) => (
+                                <ContactCard key={card.id} icon={card.icon} title={card.title} name={card.name} />
+                            ))}
                         </div>
                     </div>
-                    <div className="w-full">
-                        <form action="" className="flex flex-col gap-2 md:w-1/2">
-                            <input className="px-4 py-1 bg-[#F1F1F1]" type="text" name="Name" />
-                            <input className="px-4 py-1 bg-[#F1F1F1]" type="text" name="Email" />
-                            <textarea className="px-4 py-1 bg-[#F1F1F1]" name="" id=""></textarea>
+
+                    <div className="md:w-1/2 bg-[#E9E9E9]/80 px-4 py-4 rounded-xl">
+                        <form className="flex flex-col gap-2">
+                            <input className="px-4 py-2 bg-[#F1F1F1] rounded-md" type="text" name="Name" placeholder="Name" />
+                            <input className="px-4 py-2 bg-[#F1F1F1] rounded-md" type="email" name="Email" placeholder="Email" />
+                            <textarea className="px-4 py-2 bg-[#F1F1F1] rounded-md min-h-50" name="Message" placeholder="Message"></textarea>
+                            <button type="submit" className="bg-white text-black py-2 rounded-md hover:bg-white/80 transition">Submit</button>
                         </form>
                     </div>
                 </div>
-
             </div>
         </section>
     )
