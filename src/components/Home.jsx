aa173b90070fe4f49c7944ccc608a2294eb9df56
PrithiviPM2580 <prithivipemimagar>
2025-11-09 22:57:47 +0545

    feat: add button icons constants and integrate into About component; include download SVG in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,8 @@ import { Button } from "./ui/button"
 
 const Home = () => {
     return (
-        <section className="w-full px-4 py-8 sm:py-12">
-            <div className="max-w-4xl mx-auto flex flex-col items-center gap-4 md:gap-6 lg:gap-8">
+        <section className="w-full px-4 py-[6em] md:py-8 ">
+            <div className="max-w-4xl mx-auto flex flex-col items-center gap-6 md:gap-6 lg:gap-8">
                 <div className="bg-white rounded-3xl md:rounded-4xl flex items-center justify-center px-3 py-2 md:px-6 md:py-2">
                     <p className="text-sm md:text-base text-center">👋 Hi, I’m Prithivi Pemi Magar</p>
                 </div>
@@ -42,7 +42,7 @@ const Home = () => {
                 <p className="w-full md:w-[80%] text-[11px] md:text-[12px] lg:text-sm text-gray-custom text-center">As a backend developer, I build efficient systems that bring ideas to life — crafting RESTful APIs and optimizing databases for smooth, reliable performance.</p>
                 <div className="btn flex gap-4">
                     <Button className="rounded-full cursor-pointer">Lets Connect 💬</Button>
-                    <Button className="rounded-full border cursor-pointer border-gray-300 bg-transparent text-black">Download CV 📄</Button>
+                    <Button className="rounded-full border cursor-pointer border-gray-300 bg-transparent text-black">Download CV <span className="w-4 h-4"><img className="w-full h-full object-cover" src="/svgs/download.svg" alt="" /></span></Button>
                 </div>
             </div>
         </section>
---
+++ src/constants/index.js
@@ -0,0 +1,56 @@
+export const buttonIcons = [
+  {
+    icon: "/images/figma.png",
+    name: "Figma",
+    bg: "#ffc795",
+    rotate: 10,
+    pos: {
+      md: "md:top-[25%] md:left-[82%]",
+    },
+  },
+  {
+    icon: "/images/vscode.png",
+    name: "VSCode",
+    bg: "#79CCFF",
+    rotate: -10,
+    pos: {
+      md: "md:top-[-20%] md:left-[79%]",
+    },
+  },
+  {
+    icon: "/images/nodejs.png",
+    name: "Node.js",
+    bg: "#209651",
+    rotate: 10,
+    pos: {
+      md: "md:top-[-20%] md:left-[6%]",
+    },
+  },
+  {
+    icon: "/images/express.png",
+    name: "Express",
+    bg: "#944444",
+    rotate: 5,
+    pos: {
+      md: "md:top-[30%] md:left-[4%]",
+    },
+  },
+  {
+    icon: "/images/github.png",
+    name: "GitHub",
+    bg: "#79CCFF",
+    rotate: -15,
+    pos: {
+      md: "md:top-[72%] md:left-[79%]",
+    },
+  },
+  {
+    icon: "/images/mongodb.png",
+    name: "MongoDB",
+    bg: "#25E97D",
+    rotate: -10,
+    pos: {
+      md: "md:top-[72%] md:left-[4%]",
+    },
+  },
+];
